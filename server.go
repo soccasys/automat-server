32958d8c60e9e02a26de55ab817b27e31f9a28f5
Laurent Gauthier <laurent.gauthier@soccasys.com>
2016-03-31 11:59:03 +0200

    Added an 'example' option to the command to generate an example of projet definition

--- server.go
+++ server.go
@@ -6,6 +6,8 @@ package main
 
 import (
 	"github.com/soccasys/builder"
+	"encoding/json"
+	"syscall"
 	"flag"
 	"fmt"
 	"log"
@@ -14,7 +16,18 @@ import (
 
 var project *builder.Project
 
+func printExampleProject() {
+	p := builder.NewProject()
+        p.Name = "build-automat"
+	p.AddComponent("src/github.com/builder", "https://github.com/soccasys/build-automat.git", "master")
+	p.AddComponent("src/github.com/build-automat", "https://github.com/soccasys/builder.git", "master")
+	p.AddBuildStep("Build All", ".", []string{"go", "install", "github.com/soccasys/build-automat"})
+        text, _ := json.MarshalIndent(p, "", "    ")
+	fmt.Printf("%s\n", text)
+}
+
 func main() {
+	example := flag.Bool("example",  false, "Print an example of project definition on the standatd output, and exit")
 	file := flag.String("load", "", "Load the project definition from a file")
 	//root := flag.String("root", "/tmp/builder", "Directory where the builds are run")
 	port := flag.Int("port", 8080, "HTTP port to use for the server")
@@ -22,6 +35,10 @@ func main() {
 	if !flag.Parsed() {
 		log.Fatal("Incorrect command line parameters")
 	}
+	if *example {
+		printExampleProject()
+		syscall.Exit(0)
+        }
 	project = builder.NewProject()
 	if *file != "" {
 		project.Load(*file)
